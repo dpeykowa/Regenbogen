b05b79ccb2f203dc84aeb3680971f850b9b07001
dpeykowa <34142285+dpeykowa@users.noreply.github.com>
2017-12-11 11:00:35 +0200

    Create pricing.php

---
+++ pricing.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags always come first -->
+	<meta name="Keywords" content="цени,ценоразпис,немски,език,уроци,софия,индивидуални,частни,матури,гимназисти,прогимназисти,образование,деца,price,german,language,lessons,sofia,tutors,private,individual,course,classes,children,kids">
+	<meta name="Description" content="Ценоразпис на курсовете, като предлагаме съботно-неделни и индивидуални занятия">
+	<?php include 'head.php'; ?>
+    <title>Ценоразпис</title>
+
+</head>
+
+
+<body>
+   <?php include 'header.php'; ?>
+
+    <main id="main-tag" class="container">
+        <section style="text-align: center;" id="row1-models" class="row">
+            <img src="CSSfiles/images/priceList.png" alt="pricing">
+        </section>
+    </main>
+
+   <?php include 'footer.php'; ?>
+</body>
+
+</html>
