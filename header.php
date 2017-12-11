249c2d171ae63b2713c87b85de804c5142138c02
dpeykowa <34142285+dpeykowa@users.noreply.github.com>
2017-12-11 10:51:36 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,33 @@
+<header id="header" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2">
+                <div id="logo-section">
+                    <div id="logo-wrapper">
+                        <img src="images/logo2.png" alt="Our logo">
+                    </div>
+                    <p style="color: gray">Regenbogen</p>
+                </div>
+            </div>
+            <div class="col-sm-10">
+                <nav>
+                    <ul class="row header-navigation">
+                        <li class="col-sm-3 text-center">
+                            <a href="index.php"><span class="header-entry header-home"></span>Начало</a>
+                        </li>
+                        <li class="col-sm-2">
+                            <a href="pricing.php"><span class="header-entry header-models"></span>Ценоразпис</a>
+                        </li>
+                        <li class="col-sm-2">
+                            <a href="learning.php"><span class="header-entry header-learning"></span>Как Учат Децата?</a>
+                        </li>
+                        <li class="col-sm-2">
+                            <a href="kids.php"><span class="header-entry header-company"></span>За Малки Деца</a>
+                        </li>
+                        <li class="col-sm-3">
+                            <a href="contact.php"><span class="header-entry header-contact"></span>Контакти</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
