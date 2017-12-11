85ac734286d724ae0053b410dc714e437b4ff5d0
dpeykowa <34142285+dpeykowa@users.noreply.github.com>
2017-12-11 10:47:02 +0200

    Update and rename Contact.html to contact.php

---
+++ contact.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags always come first -->
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="Keywords" content="немски,език,уроци,софия,индивидуални,частни,матури,гимназисти,прогимназисти,образование,деца,german,language,lessons,sofia,tutors,private,individual,course,classes,children,kids">
+	<meta name="Description" content="Адрес София, улица Мальовоца. Немска езикова школа Regenbogen. Телефон за връзки.">
+<?php include 'head.php'; ?>
+    <title>Контакти</title>
+</head>
+
+
+<body>
+   <?php include 'header.php'; ?>
+    <main class="contact-info" id="main">
+        <div style="text-align: center;">
+            <img src="CSSfiles\images/contacts.jpg" alt="">
+        </div>
+        <section id="secondsection" class="container">
+            <article class="row">
+                <div class="col-sm-4">
+                    <div class="row">
+                        <h1 class="col-sm-4">Адрес:</h1>
+                        <div class="col-sm-8">
+                            <p>София, България</p>
+                            <p>ул. "Мальовица"</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="row">
+                        <h1 class="col-sm-4">Телефон:</h1>
+                        <div class="col-sm-8">
+                            <p>359 / 889 726 113</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="row">
+                        <h1 class="col-sm-4">Email:</h1>
+                        <div class="col-sm-8">
+                            <p>Email: diana_svetoslavova@abv.bg</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </section>
+    </main>
+
+    <footer id="footer">
+        <div class="row mainfooter">
+            <div id="motivationwords" class="col-sm-12">
+                <p>Powered by Desislava Peykova</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
+</body>
+
+</html>
