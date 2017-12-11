b1b4965c5c8b23cd1dca27958b83851de2f93065
dpeykowa <34142285+dpeykowa@users.noreply.github.com>
2017-12-11 10:49:19 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer id="footer">
+        <div class="row mainfooter">
+            <div id="motivationwords" class="col-sm-12">
+                <p>Powered by Desislava Peykova</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js " integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY " crossorigin="anonymous "></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js " integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD " crossorigin="anonymous "></script>
